56b6b6ff10df79d3dbfd23cb433b6138526cdaaf
frisyk <frisnadi1@gmail.com>
2024-03-12 21:04:55 +0700

    fixing bug

--- app/layout.jsx
+++ app/layout.jsx
@@ -14,14 +14,6 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <head>
-      <link
-        rel="icon"
-        href="/"
-        type="image/png"
-        sizes="20"
-      />
-      </head>
       <body className={poppins.className + " home_bg"}>
         <Navigation/>
           {children}
