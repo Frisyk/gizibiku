3757dfef55cf5760aee9a1d645fc5cf5f11ae08d
frisyk <frisnadi1@gmail.com>
2024-03-12 21:10:22 +0700

    fixing bug

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-export default function () {
+export default function Footer () {
   return (
     <div className="bg-blue-300 w-full p-5 mt-10 flex items-center justify-center" >
         <p className="text-sm text-blue-950">Gizibiku @ 2024</p>
